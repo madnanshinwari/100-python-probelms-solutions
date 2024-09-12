447f7b814e8bb2d09432b93ace436648d5d18103
Muhammad Adnan Khan <98257145+Mr-Adnankhan@users.noreply.github.com>
2024-09-12 21:55:00 +0500

    celsius to fahrenheit

---
+++ p_2.py
@@ -0,0 +1,7 @@
+# 	Write a program that will convert celsius value to fahrenheit
+
+celsius_value =int(input("Enter the Celsius value :    "))
+
+Fahrenheit_result = (celsius_value*1.8+32)
+
+print("Temperature in Fahrenhiet: ",Fahrenheit_result)
