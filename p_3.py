2727046aaf9aa1d4d589b36e0fbaba72d8d6467c
Muhammad Adnan Khan <98257145+Mr-Adnankhan@users.noreply.github.com>
2024-09-13 06:22:46 +0500

    swap the numbers

---
+++ p_3.py
@@ -0,0 +1,7 @@
+# 	User will input (2numbers).Write a program to swap the numbers
+
+num1 = 23
+num2 = 77
+num1,num2 = num2,num1
+print(f'Actual value of num1 is 23 swap value is : {num1}')
+print(f'Actual value of num2 is 77 swap value is : {num2}')
