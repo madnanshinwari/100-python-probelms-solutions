357cbe58b2e2284668dbb45d68fbbd2405bd8ebe
Muhammad Adnan Khan <98257145+Mr-Adnankhan@users.noreply.github.com>
2024-09-13 06:30:13 +0500

    sum of three digits

---
+++ p_4.py
@@ -0,0 +1,8 @@
+# Write a program that will give you the sum of 3 digits
+
+num_1 = int(input('Enter the ist number : '))
+num_2 = int(input('Enter the second number : '))
+num_3 = int(input('Enter the  third number : '))
+
+addition = num_1+num_2+num_3
+print("The Sum of three digits is : ",addition)
